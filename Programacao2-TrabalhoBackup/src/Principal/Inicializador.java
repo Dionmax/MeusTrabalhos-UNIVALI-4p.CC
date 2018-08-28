4670161cb964fe0870a0450f26f37340c5a2061b
Dionmax Nobre <dionmaxfn@gmail.com>
2018-08-28 16:12:43 -0300

    TrabalhoM1-Stream

---
+++ Programacao2-TrabalhoBackup/src/Principal/Inicializador.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+import ClassesBackup.*;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+/**
+ *
+ * @author dionm
+ */
+public class Inicializador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+        // TODO code application logic here
+    }
+    
+}
